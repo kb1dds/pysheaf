946e57b0301a85822582ebb3a11ba3e393a8fe69
feisele <feisele@isis.vanderbilt.edu>
2015-08-28 18:35:37 -0500

    pip wants a package

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 Python Cellular Sheaf Library
 
 This is part of the [AU Sheaves Simplex Activity](http://www.american.edu/cas/darpasheaves/).
+
+The pip install follows
+https://pip.pypa.io/en/latest/reference/pip_install.html#git
+
+pip install -e git+https://github.com/phreed/pysheaf.git@master#egg
--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@
 
 from distutils.core import setup
 
-setup(name='pysheaf',
-      version='1.0',
-      description='An implementation of Sheaf',
-      author='Michael Robinson',
-      author_email='feisele@isis.vanderbilt.edu',
-      url='http://www.american.edu/cas/darpasheaves/',
-      packages=['pysheaf', 'pysheaf.command'],
+setup(name = 'pysheaf',
+      version = '1.0',
+      description = 'An implementation of Sheaf',
+      author = 'Michael Robinson',
+      author_email = 'feisele@isis.vanderbilt.edu',
+      url = 'http://www.american.edu/cas/darpasheaves/',
+      package = ['pysheaf', 'simplicialHomology']
      )
